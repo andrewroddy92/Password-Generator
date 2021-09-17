5e23d7fb07fc68e62b9af5a94b0bffad898f6f49
Andrew Roddy <andrewroddy92@yahoo.com>
2021-09-16 22:17:16 -0400

    Added password generation and character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var password = "default";
+  var password = "";
   // prompt charcter length
   var characterLength = prompt("Please provide the required character length (Number must be between 8 and 128)");
-  var badNumbar = (characterLength < 8)||(characterLength > 128);
-  while (badNumbar) {
+  var badNumber = (characterLength < 8)||(characterLength > 128);
+  while (badNumber) {
     characterLength = prompt("Error - Number must be between 8 and 128. Please provide the required character length");
-    badNumbar = (characterLength < 8)||(characterLength > 128);
+    badNumber = (characterLength < 8)||(characterLength > 128);
   }
   // prompt types of characters
   // validate character type select
   // generate password
+  var charactersUpper = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+  var charactersLower = charactersUpper.map(letter => letter.toLowerCase());
+  var numbers = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
+  // TODO: ASK ABOUT THIS
+  var characterSpecial = [...Array(9)].map((val, i) => String.fromCharCode(i + 33));
+  var allCharacters = charactersUpper.concat(charactersLower).concat(characterSpecial).concat(numbers);
+  for(var i=0; i < characterLength; i++){
+    password = password.concat(allCharacters[Math.floor(Math.random() * allCharacters.length)]);
+    
+  }
+ 
   return password;
 }
 
